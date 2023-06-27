1705e7cd20c41759a8e6d0d06a43e79d874a176a
Csaba Valyi <csaba.valyi1@gmail.com>
2023-06-27 14:07:06 +0200

    Fix module issues

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-
+import { fileURLToPath } from 'url'
 import express from 'express'
 import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 const PORT = process.env.PORT || 80
 
 app.get('/', (req, res) => {
-  const indexFilePath = path(__dirname, '/index.html')
+  const indexFilePath = path.join(__dirname, '/index.html')
   res.sendFile(indexFilePath)
 })
 
